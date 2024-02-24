645dae9ae554206aa8fc48d5315199c1fa58fdb2
Mosesomo <moseswamboga4@gmail.com>
2024-02-24 13:44:09 +0300

    python intro

---
+++ python_plp/user_input.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("What is your location? ")
+
+print("Hello {}, you are {} and you live in {}"
+      .format(name, age, location))
